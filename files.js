3fa2aa65c2eb16bf80b97511492b430d0319d988
AlexisLacombe <97172743+AlexisLacombe@users.noreply.github.com>
2025-11-17 09:48:51 +0100

    Add files via upload

---
+++ files.js
@@ -0,0 +1,53 @@
+// ============================================
+// LISTE DES FICHIERS - Généré automatiquement
+// Date de génération : 28/10/2025 22:51:02
+// Nombre de fichiers : 9
+// ============================================
+
+const filesList = [
+  {
+    name: "diagramme_MCD.jpg",
+    size: 105221,
+    date: "13/10/2025"
+  },
+  {
+    name: "diagramme_gantt.png",
+    size: 60890,
+    date: "13/10/2025"
+  },
+  {
+    name: "diagramme2.png",
+    size: 233390,
+    date: "06/10/2025"
+  },
+  {
+    name: "diagramme.png",
+    size: 136174,
+    date: "06/10/2025"
+  },
+  {
+    name: "tp2.docx",
+    size: 1221254,
+    date: "29/09/2025"
+  },
+  {
+    name: "dossier 3.docx",
+    size: 879302,
+    date: "29/09/2025"
+  },
+  {
+    name: "style.css",
+    size: 767,
+    date: "29/09/2025"
+  },
+  {
+    name: "td_PGI_exercice.docx",
+    size: 5984216,
+    date: "15/09/2025"
+  },
+  {
+    name: "TD 1 SI.docx",
+    size: 19220,
+    date: "15/09/2025"
+  }
+];
