f11afc0405980d3641c5bb22d4e131863d3b96eb
AlexisLacombe <97172743+AlexisLacombe@users.noreply.github.com>
2025-11-23 17:25:20 +0100

    Fix typo in file name in filesList

--- files1.js
+++ files1.js
@@ -6,7 +6,7 @@
 // ============================================
 
 const filesList = [
-  { name: "training she╠üma acteur flux.docx", size: 8197, date: "23/11/2025" },
+  { name: "shéma acteur flux.docx", size: 8197, date: "23/11/2025" },
   { name: "diagramme_MCD.jpg", size: 105221, date: "13/10/2025" },
   { name: "diagramme_gantt.png", size: 60890, date: "13/10/2025" },
   { name: "diagramme2.png", size: 233390, date: "06/10/2025" },
@@ -17,3 +17,4 @@ const filesList = [
   { name: "td_PGI_exercice.docx", size: 5984216, date: "15/09/2025" },
   { name: "TD 1 SI.docx", size: 19220, date: "15/09/2025" }
 ];
+
